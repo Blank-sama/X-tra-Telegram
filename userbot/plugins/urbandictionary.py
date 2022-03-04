c0958ec4a05fac587a0d81db1932c5ccac69061d
Blank <93943699+Blank-sama@users.noreply.github.com>
2022-03-04 11:17:25 +0530

    Update urbandictionary.py

--- userbot/plugins/urbandictionary.py
+++ userbot/plugins/urbandictionary.py
@@ -1,11 +1,6 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-"""Urban Dictionary
-Syntax: .ud Query"""
 from telethon import events
 import asyncurban
-from userbot.utils import admin_cmd
+from Bonten.utils import admin_cmd
 
 
 @borg.on(admin_cmd("ud (.*)"))
