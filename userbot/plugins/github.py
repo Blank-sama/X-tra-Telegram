4656acba3a2a00ac7349301a32a97f44aa0b374e
Lynncept77 <95460649+Lynncept77@users.noreply.github.com>
2022-03-04 11:45:41 +0530

    Update github.py

--- userbot/plugins/github.py
+++ userbot/plugins/github.py
@@ -2,7 +2,7 @@
 Syntax: .github USERNAME"""
 from telethon import events
 import requests
-from userbot.utils import admin_cmd
+from Bonten.utils import admin_cmd
 
 
 @borg.on(admin_cmd("github (.*)"))
